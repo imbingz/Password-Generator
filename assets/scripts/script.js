20c25d6eb63f4582eaeac8f55802bf70dcd6f424
imbingz <bingz@me.com>
2020-08-06 10:03:25 -0600

    removed missed console.log n comments typo

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,13 +21,12 @@ function generatePassword() {
   while (isNaN(parseInt(userLength, 10)) || parseInt(userLength, 10) < 8 || parseInt(userLength, 10) > 128) {
     userLength = prompt("Please enter a number as your password length. The number must be between 8 and 128, inclusive.");
   }
-  console.log(userLength);
   //  Create a variables for each users preference
   var answerNumber = confirm("Would you like numbers in your password?");
   var answerUpper = confirm("Would you like uppercase letters in your password?");
   var answerLower = confirm("Would you like lowercase letters in your password?");
   var answerSymbol = confirm("Would you like symbols in your password?");
-
+  // Create a while loop to prompt the users the same questions if users did not choose any character preference
   while (!answerNumber && !answerUpper && !answerLower && !answerSymbol) {
     console.log("2nd while loop");
     alert("You must choose at least a numer, an uppercase letter, a lowercase letter or a symbol. Please try again!");
@@ -36,10 +35,9 @@ function generatePassword() {
     answerLower = confirm("Would you like lowercase letters in your password?");
     answerSymbol = confirm("Would you like symbols in your password?");
   }
-
-  // Create a variable that will holds the preference characters
+  // Create a variable that holds the preference characters
   var allChar = "";
-  // If users answer yes to any of these preference, add that preference characters to allChar variable; otherwise, alert users a message and prompt these questions again
+  // If users answer yes to any of these preference, add that preference characters to allChar variable
   if (answerNumber) {
     var number = "0123456789";
     allChar += number;
@@ -62,7 +60,7 @@ function generatePassword() {
   for (var i = 0; i < parseInt(userLength); i++) {
   // Create a variable that holds the random number created via random method within allChar
     var char = Math.floor(Math.random() * allChar.length);
-  // Add each random char to passwordChar 
+  // Add each random character to passwordChar 
     passwordChar += allChar[char]; 
   }
   return passwordChar;
